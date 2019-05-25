675f499bc49111c9ae0680d346a9eae122117203
yifan97 <xuyifan97@gmail.com>
2019-05-25 00:36:47 -0400

    add hamburger icon and its effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    $(".fa-envelope-square").click(function(){
+    $('.fa-envelope-square').click(function(){
         $("#mail-content").toggleClass('content');
+    });
+
+    $('.fa-bars').click(function(){
+        $('.wrapper').addClass('wrap-content');
+        $('.ham-wrapper').addClass('ham-content');
+
+    });
+
+    $('.close-tab').click(function(){
+        $('.wrapper').removeClass('wrap-content');
+        $('.ham-wrapper').removeClass('ham-content');
     });
 });
